7e52935fb11fed10cad0a23a7d1bb2506375e1bb
PixarPresent <pixarpresent@gmail.com>
2025-01-07 15:45:08 +0300

    fix: type of the UserDao in UserServiceImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,13 +1,14 @@
 package jm.task.core.jdbc.service;
 
 import com.sun.xml.bind.annotation.OverrideAnnotationOf;
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+    UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         userDao.createUsersTable();
